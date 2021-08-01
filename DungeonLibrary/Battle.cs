0ecad9102a0322a2a4993d5a336bedef15dd3bfa
Jacob Roelofsz <jroelofsz@outlook.com>
2021-08-01 18:52:15 -0500

    Added base funtionality for battle sequence

---
+++ DungeonLibrary/Battle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Battle
+    {
+        public static void Attack(Character attacker, Character defender)
+        {
+            int rollDice = new Random().Next(1, 101);
+
+            if (rollDice <= attacker.CalculateHitChance() - defender.CalculateBlockChance())
+            {
+                int damage = attacker.CalculateDamage();
+                defender.Life -= damage;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{attacker.Name} dealt {damage} to {defender.Name}!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"{attacker.Name} missed {defender.Name}");
+            }
+        }//end Attack()
+
+       // public static void combat(Player player, /*Monster*/ monster)
+
+    }
+}
